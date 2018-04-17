edd095737ca4b82fce76fd5182f71280016ea365
FDigiacomo <fdigiacom@gmail.com>
2018-04-17 14:00:49 +0200

    Added Unit1 and part of Unit 2

---
+++ Code/Unit2/Unit2/Program.fs
@@ -0,0 +1,24 @@
+﻿module Program
+
+open System
+
+let rec last = 
+  fun (l : List<'a>) ->
+    if l.Length = 0 then
+      failwith "The list is empty"
+    elif l.Length = 1 then 
+      l.Head
+    else
+      last l.Tail
+
+let rec rev : List<'a> -> List<'a> =
+  fun (l : List<'a>) ->
+    if l.Length = 0 then
+      []
+    else
+      (rev l.Tail) @ [l.Head]
+
+[<EntryPoint>]
+let main argv =
+  printfn "%A" (last [5;3;0;1;6])
+  0
