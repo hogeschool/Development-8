b81b178a85a97f41d7bc3e827d368b94c0883988
FDigiacomo <fdigiacom@gmail.com>
2018-05-31 14:46:42 +0200

    Added exercises of Unit 4

--- Code/Unit4ts/build/Unit4ts/unit4.js
+++ Code/Unit4ts/build/Unit4ts/unit4.js
@@ -49,4 +49,90 @@ exports.filterFold = function (predicate) { return function (l) {
         return predicate(x) ? unit2_1.Cons(x, l) : l;
     }; })(unit2_1.Empty())(l))(Unit2.rev);
 }; };
+exports.map2 = function (f) {
+    return function (l1) {
+        return function (l2) {
+            if (l1.length() != l2.length()) {
+                throw "The two lists must have the same length";
+            }
+            else if (l1.kind == "empty" && l2.kind == "empty") {
+                return unit2_1.Empty();
+            }
+            else if (l1.kind == "cons" && l2.kind == "cons") {
+                return unit2_1.Cons(f(l1.head)(l2.head), exports.map2(f)(l1.tail)(l2.tail));
+            }
+            else {
+                throw "Cannot happen";
+            }
+        };
+    };
+};
+exports.fold2 = function (f) {
+    return function (init) {
+        return function (l1) {
+            return function (l2) {
+                if (l1.length() != l2.length()) {
+                    throw "The two lists must have the same length";
+                }
+                else if (l1.kind == "empty" && l2.kind == "empty") {
+                    return init;
+                }
+                else if (l1.kind != "empty" && l2.kind != "empty") {
+                    var accumulator = f(init)(l1.head)(l2.head);
+                    return exports.fold2(f)(accumulator)(l1.tail)(l2.tail);
+                }
+                else {
+                    throw "Cannot happen";
+                }
+            };
+        };
+    };
+};
+exports.zip = function (l1) { return function (l2) {
+    if (l1.kind != l2.kind) {
+        throw "The two lists must have the same length";
+    }
+    else if (l1.kind == "empty" && l2.kind == "empty") {
+        return unit2_1.Empty();
+    }
+    else if (l1.kind != "empty" && l2.kind != "empty") {
+        return unit2_1.Cons(unit2_1.Tuple(l1.head, l2.head), exports.zip(l1.tail)(l2.tail));
+    }
+    else {
+        throw "Cannot happen";
+    }
+}; };
+exports.zipFold = function (l1) { return function (l2) {
+    if (l1.length() != l2.length()) {
+        throw "The two lists must have the same length";
+    }
+    else {
+        return apply(exports.fold2(function (l) { return function (x) { return function (y) { return unit2_1.Cons(unit2_1.Tuple(x, y), l); }; }; })(unit2_1.Empty())(l1)(l2))(Unit2.rev);
+    }
+}; };
+exports.map2Safe = function (f) {
+    return function (l1) {
+        return function (l2) {
+            if (l1.kind == "empty" && l2.kind == "empty") {
+                return unit2_1.Empty();
+            }
+            else if (l1.kind == "empty" && l2.kind != "empty") {
+                return exports.map(function (x) { return exports.None(); })(l2);
+            }
+            else if (l1.kind != "empty" && l2.kind == "empty") {
+                return exports.map(function (x) { return exports.None(); })(l1);
+            }
+            else if (l1.kind != "empty" && l2.kind != "empty") {
+                var newHead = exports.Some(f(l1.head)(l2.head));
+                return unit2_1.Cons(newHead, exports.map2Safe(f)(l1.tail)(l2.tail));
+            }
+            else {
+                throw "Cannot happen";
+            }
+        };
+    };
+};
+exports.flatten = function (l) {
+    return exports.fold(function (l1) { return function (x) { return Unit2.concat(l1)(x); }; })(unit2_1.Empty())(l);
+};
 //# sourceMappingURL=unit4.js.map
--- Code/Unit4ts/unit4.ts
+++ Code/Unit4ts/unit4.ts
@@ -8,8 +8,8 @@ export type Option<a> = {
   value: a
 }
 
-export let None = <a>() => { return {kind: "none"} }
-export let Some = <a>(v: a) => { return { kind: "some", value: v }}
+export let None = <a>(): Option<a> => { return {kind: "none"} }
+export let Some = <a>(v: a): Option<a> => { return { kind: "some", value: v }}
 
 export let filter = <a>(predicate: (x: a) => boolean) => (l : List<a>): List<a> => {
   if (l.kind == "empty") {
@@ -54,4 +54,95 @@ export let mapFold = <a, b>(f:(x: a) => b) => (l: List<a>): List<b> => {
 export let filterFold = <a>(predicate: (x: a) => boolean) => (l : List<a>): List<a> => {
   return apply<List<a>, List<a>>(fold((l: List<a>) => (x: a) => 
     predicate(x) ? Cons<a>(x, l) : l)(Empty<a>())(l))(Unit2.rev)
+}
+
+export let map2 =
+  <a, b, c>(f: (x: a) => (y: b) => c) =>
+           (l1: List<a>) => 
+           (l2: List<b>): List<c> => {
+  if (l1.length() != l2.length()) {
+    throw "The two lists must have the same length"
+  }
+  else if (l1.kind == "empty" && l2.kind == "empty") {
+    return Empty<c>()
+  }
+  else if (l1.kind == "cons" && l2.kind == "cons") {
+    return Cons<c>(f(l1.head)(l2.head), map2(f)(l1.tail)(l2.tail))
+  }
+  else {
+    throw "Cannot happen"
+  }
+}
+
+export let fold2 = 
+  <state, a, b>(f: (state: state) => (x: a) => (y: b) => state) =>
+               (init: state) =>
+               (l1: List<a>) => 
+               (l2: List<b>): state => {
+  if (l1.length() != l2.length()) {
+    throw "The two lists must have the same length"
+  }
+  else if (l1.kind == "empty" && l2.kind == "empty") {
+    return init
+  }
+  else if (l1.kind != "empty" && l2.kind != "empty") {
+    let accumulator = f(init)(l1.head)(l2.head)
+    return fold2(f)(accumulator)(l1.tail)(l2.tail)
+  }
+  else {
+    throw "Cannot happen"
+  }
+}
+
+export let zip = <a, b>(l1: List<a>) => (l2: List<b>): List<Tuple<a, b>> => {
+  if (l1.kind != l2.kind) {
+    throw "The two lists must have the same length"
+  }
+  else if (l1.kind == "empty" && l2.kind == "empty") {
+    return Empty<Tuple<a, b>>()
+  }
+  else if (l1.kind != "empty" && l2.kind != "empty") {
+    return Cons<Tuple<a, b>>(Tuple(l1.head, l2.head), zip<a, b>(l1.tail)(l2.tail))
+  }
+  else {
+    throw "Cannot happen"
+  }
+}
+
+export let zipFold = <a, b>(l1: List<a>) => (l2: List<b>): List<Tuple<a, b>> => {
+  if (l1.length() != l2.length()) {
+    throw "The two lists must have the same length"
+  }
+  else {
+    return apply<List<Tuple<a, b>>, List<Tuple<a, b>>>
+                (fold2((l: List<Tuple<a, b>>) => (x: a) => (y: b) => Cons<Tuple<a, b>>(Tuple(x, y), l))
+                (Empty<Tuple<a, b>>())
+                (l1)
+                (l2))(Unit2.rev)
+  }
+}
+
+export let map2Safe = <a, b, c>(f: (x: a) => (y: b) => c) =>
+                               (l1: List<a>) => 
+                               (l2: List<b>): List<Option<c>> => {
+  if (l1.kind == "empty" && l2.kind == "empty") {
+    return Empty<Option<c>>()
+  }
+  else if (l1.kind == "empty" && l2.kind != "empty") {
+    return map<b, Option<c>>((x: b) => None<c>())(l2)
+  }
+  else if (l1.kind != "empty" && l2.kind == "empty") {
+    return map<a, Option<c>>((x: a) => None<c>())(l1)
+  }
+  else if (l1.kind != "empty" && l2.kind != "empty") {
+    let newHead = Some<c>(f(l1.head)(l2.head))
+    return Cons<Option<c>>(newHead, map2Safe(f)(l1.tail)(l2.tail))
+  }
+  else {
+    throw "Cannot happen"
+  }
+}
+
+export let flatten = <a>(l: List<List<a>>): List<a> => {
+  return fold((l1: List<a>) => (x: List<a>): List<a> => Unit2.concat(l1)(x))(Empty<a>())(l)
 }
