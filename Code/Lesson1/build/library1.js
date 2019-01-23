0f8a9e8a28ec80943f43853a25758cbc940e60ac
Giuseppe Maggiore <giuseppemag@gmail.com>
2019-01-23 07:42:17 +0100

    Removed old TypeScript code samples.

--- Code/Lesson1/build/library1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var aaaNameSpace;
-(function (aaaNameSpace) {
-    aaaNameSpace.foo = function (x, y) {
-        y + x;
-    };
-})(aaaNameSpace = exports.aaaNameSpace || (exports.aaaNameSpace = {}));
-exports.aaa = aaaNameSpace;
--- Code/Lesson1/library1.ts
+++
@@ -1,8 +0,0 @@
-export module aaaNameSpace {
-
-  export let foo = (x: number, y: string) => { 
-    y + x
-  }
-}
-
-export let aaa = aaaNameSpace 
