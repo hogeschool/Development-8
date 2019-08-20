898162388633f1e5bf638f58b12f4eefccdcc7db
mabbadi <mabbadi@outlook.com>
2019-08-20 14:58:22 +0200

    Exam solutions

---
+++ Exam solutions/First.fs
@@ -0,0 +1,151 @@
+﻿module First
+
+let rec lineGaps (length : int) (gap : int) : string =
+  if length = 0 then
+    ""
+  else if length % gap = 0 then
+    (lineGaps (length - 1) gap) + " "
+  else
+    (lineGaps (length - 1) gap) + "*"
+
+let rec concat (l1 : List<'a>) (l2 : List<'a>) : List<'a> =
+  match l1,l2 with
+  | [],_ -> l2
+  | x :: xs,l2 -> x :: (concat xs l2)
+
+type CompressedElement<'a> =
+| Corrupted
+| Compressed of 'a * int
+| Uncompressed of 'a
+| Nested of List<CompressedElement<'a>>
+
+type UncompressedElement<'a> =
+| Element of 'a
+| Error of string
+
+let rec uncompress (l : List<CompressedElement<'a>> ) : List<UncompressedElement<'a>> =
+  let rec repeat (x : 'a) (n : int) : List<'a> =
+    match n with
+    | 0 -> []
+    | _ -> x :: (repeat x (n - 1))
+  match l with
+  | [] -> []
+  | x :: xs ->
+      match x with
+      | Corrupted -> (Error "Error uncompressing an element") :: (uncompress xs) 
+      | Compressed (element, number) ->
+           ((repeat element number) |> List.map Element) @ (uncompress xs)
+      | Uncompressed x -> (Element x) :: (uncompress xs)
+      | Nested l -> (uncompress l) @ (uncompress xs)
+
+type Either<'a,'b> =
+| Left of 'a
+| Right of 'b
+
+let mapChoice 
+  (l : List<'a>) 
+  (functions : List<Either<('a -> 'b),('a -> 'c)>>) : Option<List<Either<'b,'c>>> =
+  if l.Length <> functions.Length then
+    None
+  else
+    Some(
+      List.map2 (
+        fun x funChoice ->
+          match funChoice with
+          | Left f -> Left(f x)
+          | Right g -> Right(g x)) l functions
+    )
+
+type File =
+  {
+    Name : string
+    Extension : string
+  }
+  with
+    static member Create(name, extension) =
+      {
+        Name = name
+        Extension = extension
+      }
+
+type FileSystem =
+| Directory of List<File> * List<FileSystem>
+| Empty
+
+type SearchOption =
+| ByName of string
+| ByExtension of string
+
+let rec search (option : SearchOption) (fs : FileSystem)  : List<File> =
+  match fs with
+  | Empty -> []
+  | Directory (files,subdirectories) ->
+      (files |>
+        List.filter(
+          fun f ->
+            match option with
+            | ByName name -> f.Name.Contains(name)
+            | ByExtension ext -> f.Extension = ext)) @
+                (subdirectories |> ((List.map (search option)) >> (List.concat)))
+
+let testAll() =
+  printfn "1: %s" (lineGaps 7 3)
+  printfn "2: %A" (concat [9;9;9] [9;9;9;7;2])
+  let compressed =
+    [
+      Compressed('x', 5)
+      Nested
+        [
+          Compressed('y', 4)
+          Uncompressed 'x'
+          Nested
+            [
+              Compressed('y', 3)
+              Uncompressed 'z'
+              Compressed('w', 3)
+            ]
+          Compressed('x', 4)
+        ]
+      Nested
+        [
+          Compressed('A', 3)
+          Compressed('B', 2)
+          Uncompressed 'C'
+        ]
+    ]
+  printfn "3: %A" (uncompress compressed)
+  let elements = [5;0;2;4;3;15;2]
+  let functions =
+    [for x in 0..elements.Length - 1 do
+      if x % 2 = 0 then
+        yield Left(fun x -> x + 2)
+      else
+        yield Right(fun x -> sprintf "%d" (x + 3))]
+  printfn "4: %A" (mapChoice elements functions)
+  let (fileSystem : FileSystem) =
+    Directory(
+      [
+        File.Create("a","exe")
+        File.Create("ab","exe")
+        File.Create("ac","exe")
+      ],
+      [
+        Directory(
+          [
+            File.Create("t","txt")
+            File.Create("u","doc")
+          ],
+          [Empty]
+        )
+        Directory(
+          [
+            File.Create("xxx","doc")
+            File.Create("bs","doc")
+            File.Create("ax","csv")
+          ],
+          [Empty]
+        )
+      ]
+    )
+  printfn "5a: %A" (search (ByExtension "doc") fileSystem)
+  printfn "5b: %A" (search (ByName "a") fileSystem)
---
+++ Exam solutions/Retake.fs
@@ -0,0 +1,91 @@
+﻿module Retake
+
+let rec allNumbersModRange (n : int) (div : int) : string = 
+  if n = 0 then
+    "0"
+  elif n % div = 0 then
+    (allNumbersModRange (n - 1) div) + " " + (string n)
+  else
+    allNumbersModRange (n - 1) div
+
+let evenOdd (l : List<int>) : List<string> =
+  l |> List.map (fun x -> if x % 2 = 0 then "even" else "odd")
+
+type Person =
+  {
+    Name : string
+    LastName : string
+    Age : int
+  }
+
+let rec overage (l : List<Person>) : List<Person> =
+  l |> List.filter (fun p -> p.Age >= 18)
+
+type Either<'a,'b> =
+| Left of 'a
+| Right of 'b
+
+type FunctionWithConversion<'a,'b,'c> =
+  {
+    Function : 'a -> Either<'b,'c>
+    Conversion1 : 'b -> 'a
+    Conversion2 : 'c -> 'a
+  }
+
+let rec functionChain 
+  (functions : List<FunctionWithConversion<'a,'b,'c>>) (input : 'a) : 'a =
+
+  functions |>
+  List.fold (fun value f ->
+                let next = f.Function value
+                match next with
+                | Left l -> f.Conversion1 l
+                | Right r -> f.Conversion2 r
+                ) input
+
+type Tree<'a> =
+| Empty
+| Tree of 'a * List<Tree<'a>>
+
+let rec treeFoldDepth (f :  'state -> 'a -> 'state) (state : 'state) (t : Tree<'a>): 'state =
+  match t with
+  | Empty -> state
+  | Tree (root,trees) ->
+      let newState =
+        trees |>
+        List.fold (fun s tree -> treeFoldDepth f s tree) state
+      f newState root
+
+
+let testAll() =
+  printfn "1: %A" (allNumbersModRange 10 3)
+  printfn "2: %A" (evenOdd [3;2;4;4;4;5;5;1;5;2;2;2;4;5])
+  printfn "3: %A" (overage 
+    [
+      {Name = "John" ; LastName = "Doe"; Age = 18}
+      {Name = "John" ; LastName = "Doe"; Age = 124}
+      {Name = "John" ; LastName = "Doe"; Age = 3}
+      {Name = "John" ; LastName = "Doe"; Age = 15}
+      {Name = "John" ; LastName = "Doe"; Age = 22}
+      {Name = "John" ; LastName = "Doe"; Age = 1}
+    ])
+  let tree =
+    Tree(3,
+      [
+        Tree(1,[Empty])
+        Tree(2,
+          [
+            Tree(1,[Empty])
+            Tree(2,[Empty])
+            Tree(3,[Empty])
+          ])
+        Tree(5,
+          [
+            Tree(4,[Empty])
+            Tree(6,[Empty])
+          ])
+      ]
+    )
+  printfn "5: %A" (treeFoldDepth (fun s x -> s + (string x)) "" tree)
+
+    
